ba6e812b10d572727481b062dc24704edb62caf7
Dr Adnan Jabbar <dradnanjabbar@gmail.com>
2026-03-02 02:43:35 +0500

    Integrate Google Analytics tracking code
    Added Google Analytics tracking script to header.

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,14 @@
 <?php include("seo.php"); ?>
 <html lang="en">
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HQ78PRNQWM"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-HQ78PRNQWM');
+</script>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?= htmlspecialchars($pageTitle) ?></title>
