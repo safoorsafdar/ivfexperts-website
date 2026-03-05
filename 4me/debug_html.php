21acf874c3c796b62df45f2d3138955fc761b306
Dr Adnan Jabbar <dradnanjabbar@gmail.com>
2026-03-05 08:15:40 +0500

    chore: remove debug scripts

--- 4me/debug_html.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'local_md5' => 'F63AB01D450964DC2F9B8CC382FE9DE6', // from my local machine
-    'server_md5' => md5_file('prescriptions_edit.php')
-]);
--- 4me/debug_sync.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'patients_view_md5_local' => md5_file('e:\Github\ivfexperts\4me\patients_view.php'),
-    'patients_view_md5_server' => md5_file('patients_view.php'),
-    'header_md5_local' => md5_file('e:\Github\ivfexperts\4me\includes\header.php'),
-    'header_md5_server' => md5_file('includes/header.php'),
-]);
